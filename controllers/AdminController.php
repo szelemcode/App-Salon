fad7b9750dfce9f481e8bff001a5b89932c67c56
sebastian Belen <jseventattoostudio@gmail.com>
2026-08-28 20:29:14 -0300

    update

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController{
         $consulta .= " LEFT OUTER JOIN citasServicios ";
         $consulta .= " ON citasServicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
-        $consulta .= " ON servicios.id=citasServicios.servicioId ";
+        $consulta .= " ON servicios.id=citasservicios.servicioId ";
         $consulta .= " WHERE fecha =  '{$fecha}' ";
 
        $citas = AdminCita::SQL($consulta);
