f059d68f1013809185d270d30922134448734ea8
sebastian Belen <jseventattoostudio@gmail.com>
2026-09-01 19:47:12 -0300

    last update

--- gulpfile.js
+++ gulpfile.js
@@ -88,4 +88,4 @@ export function dev() {
 }
 
 export default series( js, css, imagenes, dev )
-export const build = series( js, css, imagenes, dev )
+export const build = series( js, css, imagenes)
